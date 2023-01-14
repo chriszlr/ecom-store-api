6e4553d4e4ad5ed87ac3698ce35c4cd74b4973f2
Christopher Zöllner <chriszoelln@gmail.com>
2023-01-14 13:39:10 +0100

    api models

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+mongoose.set('strictQuery', true)
+import userRoutes from "./routes/user-routes.js"
+
+const app = express()
+app.use(express.json())
+
+
+app.use("/api/user", userRoutes)
+
+app.listen(process.env.PORT, () => console.log(`server listening on port ${process.env.PORT}`))
+mongoose.connect(process.env.MONGO_URI)
+.then(console.log("Connected to MongoDB"))
+.catch(err => console.log(err))
