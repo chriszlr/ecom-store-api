daaedad73d516a2784b87de74302bf3ab5605534
Christopher Zöllner <chriszoelln@gmail.com>
2023-01-15 13:29:28 +0100

    auth routes

--- server/index.js
+++ server/index.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv"
 dotenv.config()
 mongoose.set('strictQuery', true)
 import userRoutes from "./routes/user-routes.js"
-
+import authRoutes from "./routes/auth-routes.js"
 const app = express()
 app.use(express.json())
 
 
 app.use("/api/user", userRoutes)
+app.use("/api/auth/", authRoutes)
 
 app.listen(process.env.PORT, () => console.log(`server listening on port ${process.env.PORT}`))
 mongoose.connect(process.env.MONGO_URI)
---
+++ server/routes/auth-routes.js
@@ -0,0 +1,41 @@
+import express from "express"
+import User from "../models/User.js"
+import CryptoJS from "crypto-js"
+import jwt from "jsonwebtoken"
+
+
+const router = express.Router()
+
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    })
+
+    try {
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(401).json("Wrong credentials!")
+
+        const storedPw = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC).toString(CryptoJS.enc.Utf8)
+        
+        storedPw !== req.body.password && res.status(401).json("Wrong credentials!")
+
+        const {password, ...others} = user._doc
+        res.status(200).json(others)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+export default router
